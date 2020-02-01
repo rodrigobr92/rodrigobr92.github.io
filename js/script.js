5b2701752e1c91fbacde1e24152b7ae593120607
Rodrigo Lima Batista <rodrigobr92@gmail.com>
2020-02-01 13:41:10 -0300

    Updated scroll

--- js/script.js
+++ js/script.js
@@ -1,58 +1,71 @@
-document.querySelector(".see-more").addEventListener('click', function() {
-    goDown(this.parentElement);
+document.querySelector(".see-more").addEventListener("click", function() {
+	goDown(this.parentElement);
 });
 
-window.addEventListener('scroll', function (e) {
-    if(this.window.innerWidth < 1279) {
-      AOS.init({disable: false});
-    }
+window.addEventListener("scroll", function(e) {
+	if (this.window.innerWidth < 1279) {
+		AOS.init({ disable: false });
+	}
 });
-
-window.addEventListener('wheel', function (e) {
-    this.console.log(this.window.innerWidth);
-    if(this.window.innerWidth > 1280) {
-      AOS.init({disable: true});
-      var el = document.querySelector(".block.opened");
-      if (e.deltaY < 0) {
-          if (el.previousElementSibling) {
-              if (el.previousElementSibling.classList.contains('block')) {
-                  clearTimeLoop(function () {
-                      goUp(el)
-                  }, 500);
-              }
-          }
-      } else if (e.deltaY > 0) {
-          if (el.nextElementSibling) {
-              if (el.nextElementSibling.classList.contains('block')) {
-                  clearTimeLoop(function () {
-                      goDown(el)
-                  }, 500);
-              }
-          }
-      }
-    }
-});
-
-// Função para limpar o timeout
-function clearTimeLoop(runFunction, time) {
-    var doit;
-    clearTimeout(doit);
-    doit = setTimeout(function () {
-        runFunction();
-    }, time);
-}
 
 // Função para mover o bloco pra cima
-function goUp(el) {
-    el.classList.remove('opened');
-    el.previousElementSibling.classList.add('opened');
-    el.previousElementSibling.classList.remove('closed');
+function goUp(el, e) {
+	console.log(e.deltaY);
+	window.removeEventListener("wheel", changePage);
+
+	el.classList.remove("opened");
+	el.previousElementSibling.classList.add("opened");
+	el.previousElementSibling.classList.remove("closed");
+
+	clearTimeLoop(function() {
+		window.addEventListener("wheel", changePage, { passive: true });
+	}, 1500);
 }
 
 // Função para mover o bloco pra baixo
-function goDown(el) {
-    el.classList.remove('opened');
-    el.classList.add('closed');
-    el.nextElementSibling.classList.add('opened');
-    el.nextElementSibling.classList.remove('closed');
+function goDown(el, e) {
+	console.log(e.deltaY);
+	window.removeEventListener("wheel", changePage);
+
+	el.classList.remove("opened");
+	el.classList.add("closed");
+	el.nextElementSibling.classList.add("opened");
+	el.nextElementSibling.classList.remove("closed");
+
+	clearTimeLoop(function() {
+		window.addEventListener("wheel", changePage, { passive: true });
+	}, 1500);
+}
+
+function changePage() {
+	var e = this.event;
+	console.log(e);
+	if (window.innerWidth > 1280) {
+		AOS.init({ disable: true });
+		var el = document.querySelector(".block.opened");
+		if (e.deltaY < 0) {
+			if (el.previousElementSibling) {
+				if (el.previousElementSibling.classList.contains("block")) {
+					goUp(el, e);
+				}
+			}
+		} else if (e.deltaY > 0) {
+			if (el.nextElementSibling) {
+				if (el.nextElementSibling.classList.contains("block")) {
+					goDown(el, e);
+				}
+			}
+		}
+	}
+}
+
+window.addEventListener("wheel", changePage, { passive: true });
+
+// Função para limpar o timeout
+function clearTimeLoop(runFunction, time) {
+	var doit;
+	clearTimeout(doit);
+	doit = setTimeout(function() {
+		runFunction();
+	}, time);
 }
